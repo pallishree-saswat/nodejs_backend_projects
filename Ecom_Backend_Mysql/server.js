f2229f6222b14147fc68b82d71c8110db0e5caa6
pallishree-saswat <saswataeroarya@gmail.com>
2022-01-08 16:50:59 +0530

    added error handling middlewares

--- Ecom_Backend_Mysql/server.js
+++ Ecom_Backend_Mysql/server.js
@@ -4,8 +4,6 @@ const morgan = require("morgan");
 const path = require("path");
 const app = express();
 
-
-
 const env = require("dotenv");
 env.config();
 const cors = require("cors");
@@ -35,6 +33,23 @@ app.use(cookieParser());
 app.use("/api/user", require("./routes/auth"));
 app.use("/api/product", require("./routes/product"));
 app.use("/api/category", require("./routes/category"));
+
+//Catching 404 Error
+app.use((req, res, next) => {
+  const error = new Error("INVALID ROUTE");
+  error.status = 404;
+  next(error);
+});
+
+//Error handler function
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`server is running on ${process.env.PORT}`);
